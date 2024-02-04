9e5ac483dc94b360206cc95506f157bedeb17a7c
himaloyash <himaloyashis98@gmail.com>
2024-02-04 23:14:34 +0600

    milestone . name

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
 const milestonesData = JSON.parse(data).data;
+console.log(milestonesData);
 
 function loadMileStones() {
  const milestones = document.querySelector('.milestones')
 
- milestones.innerHTML = `${milestonesData.map()}`
+ milestones.innerHTML = `${milestonesData.map( function (milestone){
+    return `<div class="milestone border-b">
+    <div class="flex">
+      <div class="checkbox"><input type="checkbox" /></div>
+      <div>
+        <p>
+          ${milestone.name}
+          <span><i class="fas fa-chevron-down"></i></span>
+        </p>
+      </div>
+    </div>
+    <div class="hidden_panel">
+      <div class="module border-b">
+        <p>Module Name</p>
+      </div>
+    </div>
+  </div>`
+ })}`
 
 }
