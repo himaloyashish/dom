c240d8d1e34179a03cedb3889504567cccb5b327
himaloyash <himaloyashis98@gmail.com>
2024-02-05 21:45:49 +0600

    elementNode added

--- js/main.js
+++ js/main.js
@@ -2,30 +2,34 @@ const milestonesData = JSON.parse(data).data;
 
 
 function loadMileStones() {
-    const milestones = document.querySelector('.milestones')
+  const milestones = document.querySelector('.milestones')
 
-    milestones.innerHTML = `${milestonesData.map(function (milestone) {
-        return `<div class="milestone border-b">
+  milestones.innerHTML = `${milestonesData.map(function (milestone) {
+    return `<div class="milestone border-b">
     <div class="flex">
       <div class="checkbox"><input type="checkbox" /></div>
-      <div>
+      <div onclick="openModule(this)">
         <p>
           ${milestone.name}
           <span><i class="fas fa-chevron-down"></i></span>
         </p>
       </div>
     </div>
-    <div class="hidden_panel">
+    <div class="hidden_panel show">
       ${milestone.modules.map(function (module) {
 
-            return `<div class="module border-b">
+      return `<div class="module border-b ">
         <p>${module.name}</p>
       </div>`
-        }).join("") }
+    }).join("")}
     </div>
   </div>`
-    }).join("")}`
+  }).join("")}`
 
+}
+
+function openModule(moduleElement) {
+  const moduleNode = moduleElement.parentNode.NextElementSibling;
 }
 
 loadMileStones()
